4feff2e23ddf931f7b2dc4a897576f1143659bca
jingtian1989 <zjt415991957@qq.com>
2014-03-03 21:16:55 +0800

    Update

---
+++ demo/proc/display_env.c
@@ -0,0 +1,15 @@
+#include "light.h"
+
+extern char **environ;
+
+int main(int argc, char *argv[])
+{
+	char **ep;
+
+	for (ep = environ; *ep != NULL; ep++)
+	{
+		puts(*ep);
+	}
+
+	exit(EXIT_SUCCESS);
+}
